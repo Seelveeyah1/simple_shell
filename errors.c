91a6d67254a300ccce71c07229de84799a4d662a
Hawkeyezz <clintonetuk@gmail.com>
2023-09-19 14:08:39 +0100

    print strings

---
+++ errors.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+/**
+ *_eputs - to print an input string
+ * @str: string to be printed
+ * Return: Nothing
+ */
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+ * _eputchar - to write the character c to the stderr
+ * @c: character to be printed
+ * Return: 1 on success, -1 on error, and errno is set appropriately.
+ */
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - to write the character c to given fd
+ * @c: character to be print
+ * @fd: filedescriptor to write to
+ * Return: 1 on success, -1 on error, and errno is set appropriately.
+ */
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *_putsfd - to print an input string
+ * @str: string to be printed
+ * @fd: filedescriptor to write to
+ * Return: the number of characters put
+ */
+int _putsfd(char *str, int fd)
+{
+	int i = 0;
+
+	if (!str)
+		return (0);
+	while (*str)
+	{
+		i += _putfd(*str++, fd);
+	}
+	return (i);
+}
